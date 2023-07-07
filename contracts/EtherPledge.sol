b21de26ec69fb772fadfc125f1668145ab21ac80
Jannis Maier <jannismaier97@gmail.com>
2023-07-07 11:51:00 +0200

    Fix contract deployment (#20)

--- contracts/EtherPledge.sol
+++ contracts/EtherPledge.sol
@@ -19,15 +19,20 @@ contract Campaign is Ownable {
     }
     CampaignStatus public status;
 
+    struct Feature {
+        string description;
+        uint256 votes;
+    }
+    mapping(uint256 => Feature) public features; // ID mapped to Feature
+    uint256 public featureCount; // To keep track of the number of features.
+
     mapping(address => uint256) public balances;
-    mapping(address => uint256) public votes;
-    address[] public features;
 
     // ------------------ EVENTS ------------------
 
     // Event emitted when a feature is added during construction of the contract.
     // This should be received by the frontend to display the feature and its description.
-    event FeatureAdded(address indexed featureAddress, string description);
+    event FeatureAdded(uint256 indexed featureId, string description);
     // Event emitted when a user pledges ETH to the contract.
     // This should be received by the frontend to link it with the user account.
     event Pledged(
@@ -37,7 +42,7 @@ contract Campaign is Ownable {
     );
     // Event emitted when a user votes for a feature. This should be received by the
     // frontend to link it with the campaign.
-    event Voted(address indexed voter, uint256 tokens, address feature);
+    event Voted(address indexed voter, uint256 tokens, uint256 featureId);
 
     // ------------------ FUNCTIONS ------------------
 
@@ -46,21 +51,20 @@ contract Campaign is Ownable {
         VotingToken _token,
         uint256 _goal,
         uint256 _duration,
-        address[] memory _features,
         string[] memory _descriptions
     ) {
-        require(
-            _features.length == _descriptions.length,
-            "Features and descriptions should have same length"
-        );
-
         token = _token;
         goal = _goal;
         end = block.timestamp + _duration;
-        features = _features;
-        for (uint256 i = 0; i < _features.length; i++) {
-            emit FeatureAdded(_features[i], _descriptions[i]);
+        for (uint256 i = 0; i < _descriptions.length; i++) {
+            addFeature(_descriptions[i]);
         }
+    }
+
+    function addFeature(string memory description) internal {
+        featureCount++;
+        features[featureCount] = Feature(description, 0);
+        emit FeatureAdded(featureCount, description);
     }
 
     // Function to pledge ETH to the contract. Emits the Pledged event after the state changes.
@@ -76,25 +80,14 @@ contract Campaign is Ownable {
     }
 
     // Function to vote for a feature. Emits the Voted event after the state changes.
-    function vote(uint256 tokens, address feature) external {
+    function vote(uint256 tokens, uint256 featureId) external {
         require(tokens <= token.balanceOf(msg.sender), "Not enough tokens");
-        require(isValidFeature(feature), "Invalid feature");
+        require(featureId <= featureCount && featureId > 0, "Invalid feature");
 
         token.transferFrom(msg.sender, address(this), tokens);
-        votes[feature] += tokens;
+        features[featureId].votes += tokens;
 
-        // Emit the event after the state changes
-        emit Voted(msg.sender, tokens, feature);
-    }
-
-    // Check the given feature against the list of features provided during construction
-    function isValidFeature(address feature) public view returns (bool) {
-        for (uint i = 0; i < features.length; i++) {
-            if (features[i] == feature) {
-                return true;
-            }
-        }
-        return false;
+        emit Voted(msg.sender, tokens, featureId);
     }
 
     // Function for the owner to collect all the funds once the campaign is successful
